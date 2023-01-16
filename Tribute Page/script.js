01dfd65eb4b88e61a3ba0b4f5e90a54e80abe318
Girish Ladha <96856484+girishladha@users.noreply.github.com>
2023-01-16 14:19:50 +0530

    Add files via upload

---
+++ Tribute Page/script.js
@@ -0,0 +1,12 @@
+const element = document.getElementById("button");
+element.addEventListener("click",wikipedia);
+
+
+
+
+ function wikipedia(){
+    console.warn("you are rendering to wikipedia to search a content");
+    
+
+}
+
